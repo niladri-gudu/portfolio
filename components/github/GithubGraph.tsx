e53073670615c2789013d23bbded8e422f749b94
Niladri-Gudu <niladrigudu@gmail.com>
2026-01-14 18:33:06 +0530

    chaged github calendar margin issue

--- components/github/GithubGraph.tsx
+++ components/github/GithubGraph.tsx
@@ -14,7 +14,7 @@ export default function GithubGraph() {
         GitHub Activity ({currentYear})
       </h2>
 
-      <div className="rounded-2xl border border-neutral-200 dark:border-neutral-800 bg-white dark:bg-neutral-900 p-4 overflow-x-auto overflow-y-hidden">
+      <div className="rounded-2xl border border-neutral-200 dark:border-neutral-800 bg-white dark:bg-neutral-900 p-4 overflow-x-auto overflow-y-hidden mx-2">
         <GitHubCalendar
           username="niladri-gudu"
           year={currentYear}
