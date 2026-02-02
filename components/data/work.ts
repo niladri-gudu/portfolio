67dd091798e06a91603792837dab1fc686bf2613
Niladri-Gudu <niladrigudu@gmail.com>
2026-02-02 21:41:34 +0530

    added baatasari duplicates

--- components/data/work.ts
+++ components/data/work.ts
@@ -83,32 +83,4 @@ export const workItems: WorkItem[] = [
       { name: "Vercel", icon: "vercel", link: "https://vercel.com" },
     ],
   },
-  {
-    company: "Baatasari",
-    role: "Backend Developer",
-    duration: "December 2024 - Present",
-    location: "Visakhapatnam, India (On-Site)",
-    companyLogo: 'baatasari',
-
-    links: {
-      website: "https://baatasari.com",
-      // twitter: "https://x.com/vercel",
-      linkedin: "https://www.linkedin.com/company/baatasari",
-      // github: "https://github.com/vercel",
-    },
-
-    summary: [
-      "Built scalable and reusable UI components using Next.js + Tailwind.",
-      "Improved performance and UI consistency across pages.",
-      "Worked closely with designers to deliver high-fidelity interfaces.",
-    ],
-
-    technologies: [
-      { name: "Next.js", icon: "next", link: "https://nextjs.org" },
-      { name: "TypeScript", icon: "ts", link: "https://www.typescriptlang.org" },
-      { name: "Tailwind CSS", icon: "tailwind", link: "https://tailwindcss.com" },
-      { name: "Shadcn UI", icon: "shadcn", link: "https://ui.shadcn.com" },
-      { name: "Vercel", icon: "vercel", link: "https://vercel.com" },
-    ],
-  },
 ];
