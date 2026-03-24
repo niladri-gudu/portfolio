10962bf576df2c5d806e15a7726fdb3086c0fd56
Niladri-Gudu <niladrigudu@gmail.com>
2026-03-24 16:53:13 +0530

    changed next logo theme

--- components/icons/techLogos/Next.tsx
+++ components/icons/techLogos/Next.tsx
@@ -3,18 +3,20 @@ import { SVGProps } from "react";
 export const Next = (props: SVGProps<SVGSVGElement>) => (
   <svg
     viewBox="0 0 128 128"
-    {...props}
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
+    // 'fill' here sets the default for the circle
+    fill="currentColor"
   >
-    <circle cx="64" cy="64" r="64"></circle>
+    <circle cx="64" cy="64" r="64" />
     <path
-      fill="url(#a)"
+      fill="url(#next-gradient-a)"
       d="M106.317 112.014 49.167 38.4H38.4v51.179h8.614v-40.24l52.54 67.884a64.216 64.216 0 0 0 6.763-5.209z"
-    ></path>
-    <path fill="url(#b)" d="M81.778 38.4h8.533v51.2h-8.533z"></path>
+    />
+    <path fill="url(#next-gradient-b)" d="M81.778 38.4h8.533v51.2h-8.533z" />
     <defs>
       <linearGradient
-        id="a"
+        id="next-gradient-a"
         x1="109"
         x2="144.5"
         y1="116.5"
@@ -22,11 +24,12 @@ export const Next = (props: SVGProps<SVGSVGElement>) => (
         gradientTransform="scale(.71111)"
         gradientUnits="userSpaceOnUse"
       >
-        <stop stopColor="#fff"></stop>
-        <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
+        {/* stopColor now points to our custom variable */}
+        <stop stopColor="var(--next-n-color, white)" />
+        <stop offset="1" stopColor="var(--next-n-color, white)" stopOpacity="0" />
       </linearGradient>
       <linearGradient
-        id="b"
+        id="next-gradient-b"
         x1="121"
         x2="120.799"
         y1="54"
@@ -34,8 +37,8 @@ export const Next = (props: SVGProps<SVGSVGElement>) => (
         gradientTransform="scale(.71111)"
         gradientUnits="userSpaceOnUse"
       >
-        <stop stopColor="#fff"></stop>
-        <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
+        <stop stopColor="var(--next-n-color, white)" />
+        <stop offset="1" stopColor="var(--next-n-color, white)" stopOpacity="0" />
       </linearGradient>
     </defs>
   </svg>
